c40709344ef38dcf27e9ae289aa9d0995f3ccf1b
Toshiba Windows 7 <jessieconnralph.official@gmail.com>
2023-05-14 11:42:01 +0800

    first commit

---
+++ bot.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leaf Disease | Detection</title>
+    <link rel="stylesheet" href="style.css?v=2">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="title"><i class="fas fa-leaf"></i> Leaf Disease Detection</div>
+        <div class="form">
+            <div class="bot-inbox inbox">
+                <div class="icon">
+                    <i class="fas fa-robot"></i>
+                </div>
+                <div class="msg-header">
+                    <p>Hello there, upload <br> image below</p>
+                </div>
+            </div>
+        </div>
+        <div class="typing-field">
+            <div class="input-data">
+                <input id="data" type="file" accept="image/*">
+                <button id="send-btn">check</button>
+            </div>
+        </div>
+    </div> 
+</body>
+</html>
